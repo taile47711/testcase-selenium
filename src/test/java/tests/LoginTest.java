359e4c94dc1b5f15ed3645b99ea5de0012af0004
taile47711 <taile47711@gmail.com>
2021-08-30 16:12:09 +0700

    tc01_log into Railway with valid username and password

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,22 +2,21 @@ package tests;
 
 import common.Constant;
 import helper.LogHelper;
-import org.slf4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import page_object.LoginPage;
 
 public class LoginTest extends BaseTest {
     LoginPage loginPage = new LoginPage();
-    String errorMessage = "There was a problem with your login and/or errors exist in your form.";
 
     @Test(description = "User can log into Railway with valid username and password")
     public void tc001_UserCanLogIntoRailwayWithValidUsernameAndPassword() {
-        LogHelper.info("ckick tab login");
+        LogHelper.info("Click tab login");
         loginPage.clickLoginTab();
         LogHelper.info("Enter valid data and click Login button");
         loginPage.login(Constant.USERNAME, Constant.PASSWORD);
         String actual = loginPage.getWellcomeMessage();
+        LogHelper.info("Compare with expected result");
 
         Assert.assertEquals(actual, "Welcome to Safe Railway", "Message is not displayed as expected");
     }
