8157b4da4eabe24ba9fe61ff9acea71995211614
taile47711 <taile47711@gmail.com>
2021-08-30 15:21:55 +0700

    update

--- src/main/java/helper/DriverHelper.java
+++ src/main/java/helper/DriverHelper.java
@@ -7,10 +7,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DriverHelper {
     private static WebDriver driver;
 
-    public static void initDrive() {
+    public static void initDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+    }
+
+    public static void quit() {
+        driver.quit();
     }
 
     public static void navigate(String url) {
@@ -20,4 +24,5 @@ public class DriverHelper {
     public static WebDriver getDriver() {
         return driver;
     }
+
 }
