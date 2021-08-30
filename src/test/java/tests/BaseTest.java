b45f264947d6ee51cc62f3f018b938548fcc04c7
taile47711 <taile47711@gmail.com>
2021-08-30 15:24:44 +0700

    update

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,2 +1,19 @@
-package tests;public class BaseTest {
+package tests;
+
+import common.Constant;
+import helper.DriverHelper;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    @BeforeMethod
+    public void setup() {
+            DriverHelper.initDriver();
+        DriverHelper.navigate(Constant.URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverHelper.quit();
+    }
 }
