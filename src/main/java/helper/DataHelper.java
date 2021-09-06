d3b9273d16f0e32f1ab82b541ca0d255108b7d04
taile47711 <taile47711@gmail.com>
2021-09-06 16:04:52 +0700

    create DataHelper

--- src/main/java/helper/DataHelper.java
+++ src/main/java/helper/DataHelper.java
@@ -1,2 +1,20 @@
-package helper;public class DataHelper {
+package helper;
+
+import com.github.javafaker.Faker;
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class DataHelper {
+    private static final Faker faker = new Faker();
+
+    public static String getRandomEmail() {
+        return faker.letterify("??????????@gmail.com");
+    }
+
+    public static String getRandomPassword(int number) {
+        return RandomStringUtils.randomAlphanumeric(number);
+    }
+
+    public static String getRandomPID(int number) {
+        return RandomStringUtils.randomNumeric(number);
+    }
 }
