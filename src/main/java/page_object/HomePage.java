6c1dd78433f80c1dcb32495dbcfb176ead85286d
taile47711 <taile47711@gmail.com>
2021-09-06 09:44:53 +0700

    create home page

--- src/main/java/page_object/HomePage.java
+++ src/main/java/page_object/HomePage.java
@@ -1,2 +1,21 @@
-package page_object;public class HomePage {
+package page_object;
+
+import helper.DriverHelper;
+import helper.ElementHelper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HomePage extends BasePage {
+    // Locator
+    private final By lblWelcomeContentAtTop = By.cssSelector("div#content h1");
+
+    // Element
+    private WebElement getLblWelcomeContentAtTop() {
+        return DriverHelper.getDriver().findElement(lblWelcomeContentAtTop);
+    }
+
+    // Method
+    public String getWelcomeContentAtTop() {
+        return this.getLblWelcomeContentAtTop().getText();
+    }
 }
