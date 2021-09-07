f76635ac99750dee464c4d50e86c4ad4ee748b52
taile47711 <taile47711@gmail.com>
2021-09-07 15:11:07 +0700

    create My ticket page

--- src/main/java/page_object/MyTicketPage.java
+++ src/main/java/page_object/MyTicketPage.java
@@ -1,2 +1,36 @@
-package page_object;public class MyTicketPage {
+package page_object;
+
+import helper.DriverHelper;
+import helper.ElementHelper;
+import helper.TicketHelper;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class MyTicketPage extends BasePage {
+    //locator
+    String btnCancelTicket = "//table[@class='MyTable']/tbody/tr//td[text()='%s']/following-sibling::td[text()='%s']/following-sibling::td/input[@value='Cancel']";
+
+    private WebElement getBtnCancelTicket(String departStation, String arriveAt) {
+        By btnCancel = By.xpath(String.format(btnCancelTicket, departStation, arriveAt));
+        return DriverHelper.getDriver().findElement(btnCancel);
+    }
+
+    //element
+    public void clickCancelButton(TicketHelper ticketHelpert) {
+        getBtnCancelTicket(ticketHelpert.getDepartFrom(), ticketHelpert.getArriveAt()).click();
+    }
+
+    //method
+    public void clickOKButton() {
+        DriverHelper.getDriver().switchTo().alert().accept();
+    }
+
+    public boolean isTicketDisplay(TicketHelper ticketHelper){
+        return ElementHelper.isTicketDisplayed(By.xpath(String.format(btnCancelTicket,
+                ticketHelper.getDepartDate(),
+                ticketHelper.getDepartFrom(),
+                ticketHelper.getArriveAt(),
+                ticketHelper.getSeat(),
+                ticketHelper.getTicketAmount())));
+    }
 }
