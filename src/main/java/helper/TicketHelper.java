5ab412c1d5ba4974da432750680cc3969ac2b429
taile47711 <taile47711@gmail.com>
2021-09-07 15:10:07 +0700

    create ticket helper

--- src/main/java/helper/TicketHelper.java
+++ src/main/java/helper/TicketHelper.java
@@ -1,2 +1,51 @@
-package helper;public class TicketHelper {
+package helper;
+
+public class TicketHelper {
+    private String departDate;
+    private String departFrom;
+    private String arriveAt;
+    private String seat;
+    private String ticketAmount;
+
+    public String getDepartDate() {
+
+        return departDate;
+    }
+
+    public String getDepartFrom() {
+
+        return departFrom;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public String getArriveAt() {
+        return arriveAt;
+    }
+
+    public String getTicketAmount() {
+        return ticketAmount;
+    }
+
+    public void setDepartDate(String departDate) {
+        this.departDate = departDate;
+    }
+
+    public void setDepartFrom(String departFrom) {
+        this.departFrom = departFrom;
+    }
+
+    public void setArriveAt(String arriveAt) {
+        this.arriveAt = arriveAt;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+
+    public void setTicketAmount(String ticketAmount) {
+        this.ticketAmount = ticketAmount;
+    }
 }
