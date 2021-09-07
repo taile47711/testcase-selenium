3f57b0f70fe3bde56849765b85530800d331eb44
taile47711 <taile47711@gmail.com>
2021-09-07 15:11:31 +0700

    update

--- src/main/java/page_object/BookTicketPage.java
+++ src/main/java/page_object/BookTicketPage.java
@@ -1,19 +1,67 @@
 package page_object;
 
 import helper.DriverHelper;
+import helper.ElementHelper;
+import helper.TicketHelper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
-public class BookTicketPage extends BasePage{
+public class BookTicketPage extends BasePage {
     //locator
-    private final By lblBookTicketTitle = By.cssSelector("div[id='page'] div[id='content'] h1[align='center']");
+    private final By drdDepartDate = By.name("Date");
+    private final By drdDepartFrom = By.name("DepartStation");
+    private final By drdArriveAt = By.name("ArriveStation");
+    private final By drdSeatType = By.name("SeatType");
+    private final By drdTicketAmount = By.name("TicketAmount");
+    private final By lblBookTicket = By.cssSelector("div [id='content'] h1[align='center']");
+    private final By btnBookTicket = By.cssSelector("input[type='submit']");
 
     //element
+    private Select getDrdDepartDate() {
+        Select select = new Select(DriverHelper.getDriver().findElement(drdDepartDate));
+        return select;
+    }
+
+    private Select getDrdDepartFrom() {
+        Select select = new Select(DriverHelper.getDriver().findElement(drdDepartFrom));
+        return select;
+    }
+
+    private Select getDrdArriveAt() {
+        Select select = new Select(DriverHelper.getDriver().findElement(drdArriveAt));
+        return select;
+    }
+
+    private Select getDrdSeatType() {
+        Select select = new Select(DriverHelper.getDriver().findElement(drdSeatType));
+        return select;
+    }
+
+    private Select getDrdTicketAmount() {
+        Select select = new Select(DriverHelper.getDriver().findElement(drdTicketAmount));
+        return select;
+    }
+
+    private WebElement getBtnBookTicket() {
+        return DriverHelper.getDriver().findElement(btnBookTicket);
+    }
+
     private WebElement getLblBookTicketTitle() {
-        return DriverHelper.getDriver().findElement(lblBookTicketTitle);
+        return DriverHelper.getDriver().findElement(lblBookTicket);
     }
 
     //method
+    public void bookTicket(TicketHelper ticket) {
+        ElementHelper.scrollToElement(getBtnBookTicket());
+        getDrdDepartDate().selectByVisibleText(ticket.getDepartDate());
+        getDrdDepartFrom().selectByVisibleText(ticket.getDepartFrom());
+        getDrdArriveAt().selectByVisibleText(ticket.getArriveAt());
+        getDrdSeatType().selectByVisibleText(ticket.getSeat());
+        getDrdTicketAmount().selectByVisibleText(ticket.getTicketAmount());
+        getBtnBookTicket().click();
+    }
+
     public String getBookTicketTitle() {
         return this.getLblBookTicketTitle().getText();
     }
